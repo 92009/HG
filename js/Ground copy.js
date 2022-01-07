4fa3f2031d4cdffa844c406a9540b9cbe209e737
92009 <88578664+92009@users.noreply.github.com>
2022-01-07 12:07:31 +0530

    Add files via upload

---
+++ js/Ground copy.js
@@ -0,0 +1,17 @@
+class Gound1{
+
+    constructor(){
+        var ground_options={
+            isStatic : true
+          }
+        
+          this.ground = Bodies.rectangle(450,190,90,20,ground_options)
+          World.add(world,this.ground);
+    }
+    display(){
+        noStroke();
+        fill(188,67,67);
+        rectMode(CENTER);
+        rect(this.ground.position.x,this.ground.position.y,900,20);
+    }
+}
